cb0e7e95ea4917008a12d5b3fd96c83bfc8e8a5a
sol <sol@smartm2m.co.kr>
2018-11-08 23:48:40 +0900

    pretty file structure

--- src/FileStructure.js
+++ src/FileStructure.js
@@ -4,8 +4,11 @@ class FileStructure extends Component{
     render() {
       const { fileList } = this.props;
       const list = fileList.map(
-          file => (<div key={file.depth + file.name}>{file.name}</div>)
-      );
+          file => {
+              let name = (file.depth !== 0? '⌞' : '') + file.name;
+              name = (file.depth <= 1? '' : '・'.repeat(file.depth - 1)) + name;
+              return (<div key={file.depth + file.name}>{name}</div>)
+      });
       return (
           <div>
               {list}
