c591f4500c69ad02a0ab337b25c595ae726acf99
5pecia1 <pdpxpd@gmail.com>
2020-03-19 21:32:39 +0900

    study onKeyPress

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -2,20 +2,28 @@ import React, { Component } from 'react';
 
 class EventPractice extends Component {
     state = {
+        username: '',
         message: ''
     }
 
     handleChagne = e => {
         this.setState({
-            message: e.target.value
+            [e.target.name]: e.target.value
         });
     }
 
     handleClick = () => {
-        alert(this.state.message);
+        alert(`${this.state.username}: ${this.state.message}`);
         this.setState({
+            username: '',
             message: ''
         });
+    }
+
+    handleKeyPress = (e) =>{
+        if (e.key === 'Enter') {
+            this.handleClick();
+        }
     }
 
     render() {
@@ -24,10 +32,18 @@ class EventPractice extends Component {
                 <h1>evnet test</h1>
                 <input
                     type="text"
+                    name="username"
+                    placeholder="input anything"
+                    value={this.state.username}
+                    onChange={this.handleChagne}
+                />
+                <input
+                    type="text"
                     name="message"
                     placeholder="input anything"
                     value={this.state.message}
                     onChange={this.handleChagne}
+                    onKeyPress={this.handleKeyPress}
                 />
                 <button onClick={this.handleClick}>확인</button>
             </div>
