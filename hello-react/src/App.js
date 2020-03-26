f621725d0ca93a17bf5b74c212934b969d88ecca
5pecia1 <pdpxpd@gmail.com>
2020-03-26 20:56:08 +0900

    study 7.3.2

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import './App.css';
-import EventPractice from './EventPractice';
-import RefSample from './RefSample';
+import React, { useState } from "react";
+import "./App.css";
+// import EventPractice from './EventPractice';
+// import RefSample from './RefSample';
+import LifeCycleSample from "./LifeCycleSample";
 
-const App = () => <RefSample />
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const App = () => {
+  const [color, setColor] = useState("#000000");
+
+  const handleClick = () => {
+    setColor(getRandomColor());
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Random Color!</button>
+      <LifeCycleSample color={color} />
+    </div>
+  );
+};
 
 export default App;
