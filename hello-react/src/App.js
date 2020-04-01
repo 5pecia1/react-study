ac47fc231ca9965a632a5de3ac07902f0524245f
5pecia1 <pdpxpd@gmail.com>
2020-04-02 02:26:33 +0900

    study 8

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,27 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 // import EventPractice from './EventPractice';
 // import RefSample from './RefSample';
-import LifeCycleSample from "./LifeCycleSample";
-import ErrorBoundary from "./ErrorBoundary";
-
-function getRandomColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// import LifeCycleSample from "./LifeCycleSample";
+// import ErrorBoundary from "./ErrorBoundary";
+import Counter from "./Counter";
 
 const App = () => {
-  const [color, setColor] = useState("#000000");
-
-  const handleClick = () => {
-    setColor(getRandomColor());
-  };
 
   return (
     <div>
-      <button onClick={handleClick}>Random Color!</button>
-      <ErrorBoundary>
-        <LifeCycleSample color={color} />
-      </ErrorBoundary>
+      <Counter />
     </div>
   );
 };
--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,36 +1,28 @@
-import React, { Component } from 'react';
+import React, { useReducer } from "react";
 
-class Counter extends Component {
-    state = {
-        number:0
-    }
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const { number } = this.state;
-        return (
-            <div>
-                <h1>{number}</h1>
-                <button 
-                    onClick={() => {
-                        this.setState((prevState, props) => 
-                            ({number: prevState.number + 1}),
-                            () =>{
-                                console.log(this.state)
-                            }
-                        );
-                        this.setState((prevState, props) => 
-                            ({number: prevState.number + 1})
-                        );
-                    }}
-                    >
-                        +1
-                    </button>
-            </div>
-        );
-    }
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
 }
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  return (
+    <div>
+        <p>
+            현재 카운터 값은 <b>{state.value}</b> 임.
+        </p>
+        <button onClick={()=> dispatch({type:'INCREMENT'})}>+1</button>
+        <button onClick={()=> dispatch({type:'DECREMENT'})}>-1</button>
+    </div>
+  );
+};
 
 export default Counter;
