6625e18e736241fd60d683250d11e0308f87eaa8
5pecia1 <pdpxpd@gmail.com>
2020-04-07 21:08:20 +0900

    study 17.5

--- react-redux-tutorial/src/components/Todos.jsx
+++ react-redux-tutorial/src/components/Todos.jsx
@@ -1,41 +1,56 @@
 import React from 'react';
 
 const TodoItem = ({ todo, onToggle, onRemove }) => {
-    return (
-        <div>
-            <input type="checkbox"/>
-            <span>example text</span>
-            <button>delete</button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <input
+        type="checkbox"
+        onClick={() => onToggle(todo.id)}
+        checked={todo.done}
+        readOnly={true}
+      />
+      <span style={{ textDecoration: todo.done ? 'line-through' : 'noe' }}>
+        {todo.text}
+      </span>
+      <button onClick={() => onRemove(todo.id)}>delete</button>
+    </div>
+  );
+};
 
 const Todos = ({
-    input,
-    todos,
-    onChangeInput,
-    onInsert,
-    onToggle,
-    onRemove,
+  input,
+  todos,
+  onChangeInput,
+  onInsert,
+  onToggle,
+  onRemove,
 }) => {
-    const onSubmit = e => {
-        e.preventDefault();
-    };
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input/>
-                <button type="submit">submit</button>
-            </form>
-            <div>
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
-            </div>
-        </div>
-    );
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onInsert(input);
+    onChangeInput('');
+  };
+  
+  const onChange = (e) => onChangeInput(e.target.value);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={input} onChange={onChange} />
+        <button type="submit">submit</button>
+      </form>
+      <div>
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            onToggle={onToggle}
+            onRemove={onRemove}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Todos;
