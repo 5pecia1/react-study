dec5883877bc36576798493e16d22d337a39d69f
5pecia1 <pdpxpd@gmail.com>
2020-04-08 04:28:18 +0900

    study 17.7.2

--- react-redux-tutorial/src/containers/CounterContainer.jsx
+++ react-redux-tutorial/src/containers/CounterContainer.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+// const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter
+      number={number}
+      // onIncrease={() => dispatch(increase())}
+      // onDecrease={() => dispatch(decrease())}
+      onIncrease={useCallback(() => dispatch(increase()), [dispatch])}
+      onDecrease={useCallback(() => dispatch(decrease()), [dispatch])}
+    />
   );
 };
 
@@ -39,12 +48,14 @@ const CounterContainer = ({ number, increase, decrease }) => {
 //     ),
 // )(CounterContainer);
 
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   {
+//     increase,
+//     decrease,
+//   },
+// )(CounterContainer);
+
+export default CounterContainer;
